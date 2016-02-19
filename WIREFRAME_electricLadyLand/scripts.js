ca76451f56d58b183389e7f84f9c41645b423bd4
Terrell Quillin <terrellq@gmail.com>
2016-02-19 11:47:14 -0700

    Adds wireframe files.

---
+++ WIREFRAME_electricLadyLand/scripts.js
@@ -0,0 +1,12 @@
+console.log("on est là?");
+
+// $(document).ready(function(){
+//   $('.prices').hover(function(){
+//     alert('a gram is enough for one normal joint (white paper) or half a blunt (swisha papers)');
+//
+//   }, 300);
+//
+//
+//
+//
+// });
